9c62d2a4fbc2f707ab5295ffd2b7eda3c2b9c4f8
Davyd Bredykhin <dbredykh@c2r8s2.42malaga.com>
2023-09-15 11:36:47 +0200

    ft_prinf for exam file .c

---
+++ ft_printf_for_exam.c
@@ -0,0 +1,96 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+int	ft_putdec(int num)
+{
+	int	len;
+
+	if (num == 0)
+		return (ft_putchar('0'));
+	if (num < 0)
+	{
+		ft_putchar('-');
+		return (1 + (ft_putdec(-num)));
+	}
+	if (num >= 10)
+	{
+		len = ft_putdec(num / 10);
+		ft_putchar((num % 10) + '0');
+		return (len + 1);
+	}
+	else
+	{
+		return (ft_putchar(num + '0'));
+	}
+}
+int	ft_puthexa(unsigned long long n)
+{
+	unsigned long long	count;
+
+	count = 0;
+	if (n >= 16)
+		count += ft_puthexa(n / 16);
+	ft_putchar("0123456789abcdef"[n % 16]);
+	count++;
+	return (count);
+}
+int	ft_types(const char c, va_list param)
+{
+	int	count;
+
+	count = 0;
+	if (c == 's')
+		count += ft_putstr(va_arg(param, char *));
+	if (c == 'd')
+		count += ft_putdec(va_arg(param, int));
+	if (c == 'x')
+		count += ft_puthexa(va_arg(param, unsigned int));
+	return (count);
+}
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		count;
+	va_list	param;
+
+	va_start(param, str);
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] != '%')
+			count += ft_putchar(str[i]);
+		else
+		{
+			i++;
+			count += ft_types(str[i], param);
+		}
+		i++;
+	}
+	va_end(param);
+	return (count);
+}
+int	main(void)
+{
+	printf("%d\n", ft_printf("Hexadecimal for num: %d is hex: %x\n string: %s",
+			42, 42, "String"));
+	return (0);
+}
