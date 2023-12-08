f41fd94a61006e86d0e77eb86f14705266664395
Davyd Bredykhin <81176650+BredyByte@users.noreply.github.com>
2023-12-08 23:30:18 +0100

    fix: Code for exam is fixed, now is OK

--- ft_printf_for_exam.c
+++ ft_printf_for_exam.c
@@ -3,97 +3,55 @@
 #include <unistd.h>
 
 
-
-int	ft_putchar(int c)
+void putstr(char *str, int *count)
 {
-	write(1, &c, 1);
-	return (2);
+	if (!str)
+		str = "(null)";
+	while (*str)
+		*count += write(1, str++, 1);
 }
-int	ft_putstr(char *str)
-{
-	int	i;
 
-	i = 0;
-	while (str[i])
+void putbase(long long int n, int base, int *count)
+{
+	if (n < 0)
 	{
-		write(1, &str[i], 1);
-		i++;
+		*count += write(1, "-", 1);
+		n *= -1;
 	}
-	return (123);
+	if (n >= base)
+		putbase(n / base, base, count);
+	*count += write(1, &"0123456789abcdef"[n % base], 1);
 }
-    write(1, "q", 1);
-int	ft_putdec(int num)
-{
-	int	len;
 
-	if (num == 0)
-		return (ft_putchar('0'));
-	if (num < 0)
-	{
-		ft_putchar('-');
-		return (1 + (ft_putdec(-num)));
-	}
-	if (num >= 10)
-	{
-		len = ft_putdec(num / 10);
-		ft_putchar((num % 10) + '0');
-		return (len + 1);
-	}
-	else
-	{
-		return (ft_putchar(num + '0'));
-	}
-}
-int	ft_puthexa(unsigned long long n)
-{
-	unsigned long long	count;
-
-	count = 0;
-	if (n >= 16)
-		count += ft_puthexa(n / 16);
-	ft_putchar("0123456789abcdef"[n % 16]);
-	count++;
-	return (count);
-}
-int	ft_types(const char c, va_list param)
-{
-	int	count;
-
-	count = 0;
-	if (c == 's')
-		count += ft_putstr(va_arg(param, char *));
-	if (c == 'd')
-		count += ft_putdec(va_arg(param, int));
-	if (c == 'x')
-		count += ft_puthexa(va_arg(param, unsigned int));
-	return (count);
-}
 int	ft_printf(const char *str, ...)
 {
-	int		i;
-	int		count;
+	int		count = 0;
 	va_list	param;
 
 	va_start(param, str);
-	i = 0;
-	count = 0;
-	while (str[i])
+	while (*str)
 	{
-		if (str[i] != '%')
-			count += ft_putchar(str[i]);
-		else
+		if ((*str == '%') && ((*(str + 1) == 'd') || (*(str + 1) == 'x') || (*(str + 1) == 's')))
 		{
-			i++;
-			count += ft_types(str[i], param);
+			str++;
+			if (*str == 'd')
+				putbase((long long int)va_arg(param, int), 10, &count);
+			else if (*str == 'x')
+				putbase((long long int)va_arg(param, unsigned int), 16, &count);
+			else if (*str == 's')
+				putstr(va_arg(param, char *), &count);
 		}
-		i++;
+		else
+			count += write(1, str, 1);
+		str++;
 	}
-	va_end(param);
-	return (count);
+	return (va_end(param), count);
 }
-int	main(void)
+
+int	main()
 {
 	printf("%d\n", ft_printf("Hexadecimal for num: %d is hex: %x\n string: %s",
 			42, 42, "String"));
 	return (0);
 }
+
